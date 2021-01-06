279881f49e5e334809a998d26c79d78742f8299a
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-06 22:45:14 +0530

    Update and rename Number of Steps to Reduce a Number to Zero to Number-of-Steps-to-Reduce-a-Number-to-Zero.py

--- Number-of-Steps-to-Reduce-a-Number-to-Zero.py
+++ Number-of-Steps-to-Reduce-a-Number-to-Zero.py
@@ -1,6 +1,6 @@
-Problem:
-Given a non-negative integer num, return the number of steps to reduce it to zero.
-If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+# Problem:
+# Given a non-negative integer num, return the number of steps to reduce it to zero.
+# If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 
 Solution:
 class Solution:
