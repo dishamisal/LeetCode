b4827b78e63fb4edc28351510e491513df09a2e2
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-06 21:27:59 +0530

    Update and rename Unique Number of Occurrences to Unique-Number-of-Occurrences.py

--- Unique-Number-of-Occurrences.py
+++ Unique-Number-of-Occurrences.py
@@ -1,5 +1,5 @@
-Problem:
-Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+# Problem:
+# Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
 
 Solution:
 class Solution:
