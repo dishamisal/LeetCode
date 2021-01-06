798a4be5725f54fbbd2f14666294617f89fded50
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-06 22:46:23 +0530

    Update and rename Kids With the Greatest Number of Candies to Kids-With-the-Greatest-Number-of-Candies.py

--- Kids-With-the-Greatest-Number-of-Candies.py
+++ Kids-With-the-Greatest-Number-of-Candies.py
@@ -1,7 +1,7 @@
-Problem:
-Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
-For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them.
-Notice that multiple kids can have the greatest number of candies.
+# Problem:
+# Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+# For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them.
+# Notice that multiple kids can have the greatest number of candies.
 
 Solution:
 class Solution:
