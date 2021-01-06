5590f9fc1911ec7826beaeee84aee1e82704b20a
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-06 22:48:07 +0530

    Update and rename Shuffle the Array to Shuffle-the-Array.py

--- Shuffle-the-Array.py
+++ Shuffle-the-Array.py
@@ -1,6 +1,6 @@
-Problem:
-Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
-Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+# Problem:
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
 Solution:
 class Solution:
