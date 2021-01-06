379f9b167c1fab7569c52d56de5f4447f5abe507
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-06 22:48:47 +0530

    Update and rename Shuffle String to Shuffle-String.py

--- Shuffle-String.py
+++ Shuffle-String.py
@@ -1,7 +1,7 @@
-Problem:
-Given a string s and an integer array indices of the same length.
-The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-Return the shuffled string.
+# Problem:
+# Given a string s and an integer array indices of the same length.
+# The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+# Return the shuffled string.
 
 Solution:
 class Solution:
