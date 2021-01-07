7905653e5a1007ce5041ee90ac7251f2df0e40d3
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-07 16:11:09 +0530

    Update and rename Jewels and Stones to Jewels-and-Stones.py

--- Jewels-and-Stones.py
+++ Jewels-and-Stones.py
@@ -1,6 +1,6 @@
-Problem:
-You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
-Letters are case sensitive, so "a" is considered a different type of stone from "A".
+# Problem:
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
 Solution 1:
 class Solution:
