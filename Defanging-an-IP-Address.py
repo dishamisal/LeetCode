ca21f68cd9b41d1b9a917402ee26ba3fe7559477
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-01-07 16:12:51 +0530

    Update and rename Defanging an IP Address to Defanging-an-IP-Address.py

--- Defanging-an-IP-Address.py
+++ Defanging-an-IP-Address.py
@@ -1,6 +1,6 @@
-Problem:
-Given a valid (IPv4) IP address, return a defanged version of that IP address.
-A defanged IP address replaces every period "." with "[.]".
+# Problem:
+# Given a valid (IPv4) IP address, return a defanged version of that IP address.
+# A defanged IP address replaces every period "." with "[.]".
 
 Solution:
 class Solution:
