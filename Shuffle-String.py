71d80f54a92145ce17e51926f87675234ec8fa10
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-02-06 21:56:59 +0530

    Update Shuffle-String.py

--- Shuffle-String.py
+++ Shuffle-String.py
@@ -3,7 +3,12 @@
 # The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 # Return the shuffled string.
 
-Solution:
+# Example
+# Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+# Output: "leetcode"
+# Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+# Solution:
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         t = tuple(indices + [s])
