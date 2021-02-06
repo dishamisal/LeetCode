8b17b49b00f55dd9b61ca9f3d3549f687d393aa1
Disha Misal <50451550+dishamisal@users.noreply.github.com>
2021-02-06 21:37:04 +0530

    Update Decompress-Run-Length-Encoded-List.py

--- Decompress-Run-Length-Encoded-List.py
+++ Decompress-Run-Length-Encoded-List.py
@@ -5,7 +5,14 @@
 # Concatenate all the sublists from left to right to generate the decompressed list.
 # Return the decompressed list.
 
-Solution:
+# Example
+# Input: nums = [1,2,3,4]
+# Output: [2,4,4,4]
+# Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+# The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+# At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+
+# Solution:
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         dict = {}
